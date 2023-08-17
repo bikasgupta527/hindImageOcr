a2014177de9a7d33083f2cc009a40a78db08b99a
Bikas Gupta <52459462+bikasgupta527@users.noreply.github.com>
2023-08-17 14:27:42 +0530

    adding-files

---
+++ test.py
@@ -0,0 +1,7 @@
+from hindImageOcr.hindi_image_translater import HindiToEnglishTranslator
+
+
+hindOcr = HindiToEnglishTranslator()
+
+hindOcr.process_image("D:/AST/Projects/pdf-parsing/data/compresshindibill.jpg")
+
